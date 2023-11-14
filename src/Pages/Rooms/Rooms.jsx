4bd3977af69d7e357743e405f6cb7f9c3c0132db
Rahatul-Alom <muhammadrahatulalom@gmail.com>
2023-11-14 23:43:42 +0600

    added filter option in room page without fucntionality

--- src/Pages/Rooms/Rooms.jsx
+++ src/Pages/Rooms/Rooms.jsx
@@ -1,16 +1,28 @@
-import {useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import ShowRooms from "./ShowRooms";
 
 const Rooms = () => {
-    const rooms = useLoaderData()
-    console.log(rooms)
-    return (
-        <div className="grid grid-cols-3 gap-9 mx-auto max-w-7xl mt-20">
-            {
-                rooms.map(room => <ShowRooms key={room._id} rooms = {room}></ShowRooms>)
-            }
-        </div>
-    );
+  const rooms = useLoaderData();
+  console.log(rooms);
+  return (
+    <div className="mx-auto max-w-7xl ">
+      <div className="mt-20">
+        <h1 className="font-bold my-4 text-xl">Filter with price</h1>
+        <select className="select select-bordered w-full max-w-xs">
+          <option disabled selected>
+            Who shot first?
+          </option>
+          <option>Han Solo</option>
+          <option>Greedo</option>
+        </select>
+      </div>
+      <div className="grid grid-cols-3 gap-9 mt-20">
+        {rooms.map((room) => (
+          <ShowRooms key={room._id} rooms={room}></ShowRooms>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Rooms;
--- src/Pages/Rooms/ShowRoomDetails.jsx
+++ src/Pages/Rooms/ShowRoomDetails.jsx
@@ -1,6 +1,6 @@
 import { MdAddHome } from "react-icons/md";
 const ShowRoomDetails = ({ details }) => {
-  const { image, room_title, price_per_night, size, special_offer, description } = details;
+  const { image, room_title, price_per_night, size, special_offer, description, location } = details;
   return (
     <div className="flex flex-col justify-center lg:flex-row mx-auto max-w-7xl my-20">
         <div className="">
@@ -10,6 +10,7 @@ const ShowRoomDetails = ({ details }) => {
             <h1 className="text-4xl font-bold">{room_title}</h1>
             <p className="text-lg mt-5"><b>Price per night:</b> ${price_per_night}</p>
             <p className="text-lg mt-1"><b>Room Size:</b>  {size}</p>
+            <p className="text-lg mt-1"><b>Location:</b> {location}</p>
             <p className="text-lg mt-1"><b>Special Offer:</b> {special_offer}</p>
             <p className="text-lg text-justify mt-2"><b>Description:</b> {description}</p>
             <button className="btn mt-5 bg-[#aa825d] text-white font-semibold"><MdAddHome className="text-xl" />
