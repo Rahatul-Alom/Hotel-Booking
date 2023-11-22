47fc48b43ebeb6fb34ebcc9206f4942244d54ac6
Rahatul-Alom <muhammadrahatulalom@gmail.com>
2023-11-23 00:33:44 +0600

    show room data using axios tanstack qurey

--- src/Pages/Rooms/Rooms.jsx
+++ src/Pages/Rooms/Rooms.jsx
@@ -1,15 +1,42 @@
 import { useLoaderData } from "react-router-dom";
 import ShowRooms from "./ShowRooms";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 
 const Rooms = () => {
-  const rooms = useLoaderData();
+
+  const axios = useAxiosSecure();
+  const getRooms = async ()=>{
+    const res = await  axios.get('/rooms');
+    return res;
+  }
+
+  const {
+    data: rooms, 
+    isLoading, 
+    isError,
+    error} = useQuery({
+    queryKey: ['rooms'],
+    queryFn: getRooms,
+
+  })
+  console.log(rooms)
+
+  if(isLoading){
+    return <p>Loading...</p>
+  }
+
+  if(isError){
+    return <p>something is erroe: {error}</p>
+  }
   
-  console.log(rooms);
   return (
     <div className="mx-auto max-w-7xl ">
       <div className="mt-20">
         <h1 className="font-bold my-4 text-xl">Filter with price range</h1>
         <select selected defaultValue={ 'Select Price'} className="select select-bordered w-full max-w-xs">
+        {/* onChange={(e) => setSelectedPrice(e.target.value)} */}
           <option disabled>
             Select Price
           </option>
@@ -20,7 +47,7 @@ const Rooms = () => {
         </select>
       </div>
       <div className="grid grid-cols-3 gap-9 mt-20">
-        {rooms.map((room) => (
+        {rooms?.data?.map((room) => (
           <ShowRooms key={room._id} rooms={room}></ShowRooms>
         ))}
       </div>
